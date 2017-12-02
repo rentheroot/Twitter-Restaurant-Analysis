002ef465cef8483a19fbcea8541da71a02ff1d95
xXReneeXx <rjs2018@gmail.com>
2017-12-02 10:37:59 -0500

    Added in all the access Tokens
    -added imports
    -added access tokens

---
+++ Python/ApiIntegration.py
@@ -0,0 +1,10 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "897495635682893826-BewCW8BZtcWbn0v3FIrJSAwIisXqizw"
+access_token_secret = "aggABxF67fsBSDJDkhzpBbYiUF15M0fQuPU1Xy5XqK8xT"
+consumer_key = "McoZRCrbLbONTKGVHR65qLEmF"
+consumer_secret = "ffNHti5vHkkzu5sehaGkpB4Pvntds8tKYZFMw1pQTXcqFCR05f"
