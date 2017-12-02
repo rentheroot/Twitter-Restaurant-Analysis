10c928aafb2d149d7fb903b66673beb747963b39
xXReneeXx <rjs2018@gmail.com>
2017-12-02 10:47:17 -0500

    Added in more api stuff

--- Python/ApiIntegration.py
+++ Python/ApiIntegration.py
@@ -1,4 +1,4 @@
-#Import the necessary methods from tweepy library
+#Tweepy library imports
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -6,8 +6,30 @@ from tweepy import Stream
 #Json parsing imports
 import json
 
-#Variables that contains the user credentials to access Twitter API 
+#Credentials to access Twitter API 
 access_token = "897495635682893826-BewCW8BZtcWbn0v3FIrJSAwIisXqizw"
 access_token_secret = "aggABxF67fsBSDJDkhzpBbYiUF15M0fQuPU1Xy5XqK8xT"
 consumer_key = "McoZRCrbLbONTKGVHR65qLEmF"
 consumer_secret = "ffNHti5vHkkzu5sehaGkpB4Pvntds8tKYZFMw1pQTXcqFCR05f"
+
+#print tweets to stdout
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    #Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #Filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
