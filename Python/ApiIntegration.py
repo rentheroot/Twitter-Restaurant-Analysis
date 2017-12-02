48899367f2056ad778965067fd3cbb2b63bdc82e
xXReneeXx <rjs2018@gmail.com>
2017-12-02 17:10:31 -0500

    push

--- Python/ApiIntegration.py
+++ Python/ApiIntegration.py
@@ -2,6 +2,8 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from geopy.geocoders import Nominatim 
+
 
 #Json parsing imports
 import json
@@ -11,6 +13,13 @@ access_token = "897495635682893826-BewCW8BZtcWbn0v3FIrJSAwIisXqizw"
 access_token_secret = "aggABxF67fsBSDJDkhzpBbYiUF15M0fQuPU1Xy5XqK8xT"
 consumer_key = "McoZRCrbLbONTKGVHR65qLEmF"
 consumer_secret = "ffNHti5vHkkzu5sehaGkpB4Pvntds8tKYZFMw1pQTXcqFCR05f"
+
+restaurantName = input("Name of Restaurant")
+restaurantLocation = input("Location of restaurant (city, state, country)")
+geolocator = Nominatim()
+location = geolocator.geocode(restaurantLocation)
+location= location.raw
+boundingValues = location['boundingbox']
 
 with open ('TwitterOutput.txt', 'w') as f:
     f.close()
@@ -24,25 +33,30 @@ class StdOutListener(StreamListener):
 
         
     def on_data(self, data):
-
-        all_data = json.loads(data)
         
-        tweet = all_data["text"]
-        tweet2 = tweet.replace(",", '')
-        
-        with open  ('TwitterFormatted.txt', 'a') as f1:
-            f1.write(str(tweet2))
-            f1.write(',')
-            f1.close
+        if restaurantName in status.text.lower():
+            all_data = json.loads(data)
+            
+            tweet = all_data["text"]
+            tweet2 = tweet.replace(",", '')
+            
+            with open  ('TwitterFormatted.txt', 'a') as f1:
+                try:
+                    f1.write(str(tweet2))
+                    f1.write(',')
+                    f1.close
+                except IOError:
+                    pass
+                    f1.close
 
-        with open ('TwitterOutput.txt', 'a') as f:
-            self.num_tweets += 1
-            if self.num_tweets < 5:
-                f.write(data)
-                return True
-            else:
-                return False
-                f.close()
+            with open ('TwitterOutput.txt', 'a') as f:
+                self.num_tweets += 1
+                if self.num_tweets < 5:
+                    f.write(data)
+                    return True
+                else:
+                    return False
+                    f.close()
 
     def on_error(self, status):
         print(status)
@@ -57,18 +71,10 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    restaurantName = input("Name of Restaurant")
+    #print(location.address)
+    #print((location.latitude, location.longitude))
+    
+    
     #Filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=[restaurantName])
-
-    
-
-    
-##    for line in tweets_file:
-##        try:
-##            all_data = json.loads(data)
-##            tweet = all_data["text"]
-##            print(tweet)
-##        except:
-##            continue
+    stream.filter(locations=[boundingValues])
     
